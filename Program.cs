b4137208645c93e11803a8ea364ecb6190ba3e95
sunilkumarmandati <sunilkumar.mandadi@gmail.com>
2022-01-06 17:21:38 -0800

    initinal commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            QueueConsumer.Consumer(channel);
+        }
+    }
+}
